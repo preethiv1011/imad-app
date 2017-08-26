0e76790bd36e45dd41a8196cc8fe5a3695360be9
preethiv1011 <31315760+preethiv1011@users.noreply.github.com>
2017-08-26 14:29:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req, res){
-res.send(createTemplate(articleone));
+res.send(createTemplate(article-one));
     });
     
 app.get('/article-two',function(req, res){
