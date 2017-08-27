cb0d36d7d545ce7ece1c4357153b439f48191f8b
preethiv1011 <31315760+preethiv1011@users.noreply.github.com>
2017-08-27 12:31:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,22 @@ var counter=0;
 button.onclick = function ()  {
     
     //create the request object
-                counter = counter + 1;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-     
-     
+    var request= new XMLhttprequest();
     
+        //capture the response and store it in a variable
+        request.onreadystatechange = function() {
+             if(request.readystate == XMLhttprequest.DONE){
+                 //TAKE SOME ACTION
+              if(request.status == 200){
+                    var counter = request.responseText ;
+                    var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
+              }
+             }
+          //not done yet 
+        };  
+        //make the request
+         request.open('GET','http://preethiv1011.imad.hasura-app.io/counter',true);
+         request.send(null);
+             
 };
